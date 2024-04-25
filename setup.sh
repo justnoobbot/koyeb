c267add91416846db719da3bb9ff07c1d5b4f6a3
justnoobbot <72711349+justnoobbot@users.noreply.github.com>
2024-04-25 22:09:11 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,9 @@
 
 PAT="$GITHUB_PAT"       # Github PAT (from env variables)
 
-echo $PAT
+git clone https://$PAT@github.com/justnoobbot/wp-bot.git ./apps/wpbot
 
-git clone https://$PAT@github.com/justnoobbot/wp-bot.git /apps/wpbot
-
-cd /apps/wpbot
+cd ./apps/wpbot
 npm install
 
-npm install pm2 -g
-pm2 start /apps/wpbot/index.js
+npx pm2 start ./apps/wpbot/index.js
